f6e476b4e3cffe92e018d71c4753540be7b47d59
Fabio Van Dooren jwz <fabio.vandooren@hotmail.com>
2016-05-10 22:23:04 +0200

    databank primary key en autoincrement terug bijgevoegd

--- sql/db.sql
+++ sql/db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 14, 2016 at 02:09 PM
+-- Generation Time: May 10, 2016 at 10:20 PM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.0
 
@@ -17,41 +17,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `Users` (
-  `pk_user_id` int(11) NOT NULL,
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
   `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `Image` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+  `Avatar` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `Users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `Users` (`pk_user_id`, `username`, `password`, `email`, `Image`) VALUES
-(31, 'fabio', 'ksjldhkqjfsd', 'fazedbfajk@kjdfbdgnf.com', NULL),
-(32, 'jona', 'ngfhgjhgjh', 'hkjgh', NULL);
+INSERT INTO `users` (`ID`, `username`, `password`, `email`, `Avatar`) VALUES
+(1, 'fabio', 'fabio', 'fabio.vandooren@hotmail.com', '/Applications/MAMP/htdocs/IMDStagram2/postImages/BMW E36 CLEAN.jpg'),
+(2, 'kian', 'kian', 'kiangroene@g.com', NULL),
+(3, 'jonathan', 'jonathan', 'jonathan@g.com', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `Users`
+-- Indexes for table `users`
 --
-ALTER TABLE `Users`
-  ADD PRIMARY KEY (`pk_user_id`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `Users`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `Users`
-  MODIFY `pk_user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
