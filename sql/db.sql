a5588131afa352016b2a9e75c8550e6b9887b429
Fabio Van Dooren jwz <fabio.vandooren@hotmail.com>
2016-05-15 13:05:34 +0200

    databank geupdated met prive field

--- sql/db.sql
+++ sql/db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 10, 2016 at 10:20 PM
+-- Generation Time: May 15, 2016 at 01:04 PM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.0
 
@@ -17,6 +17,63 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `ID` int(11) NOT NULL,
+  `post_ID` int(11) NOT NULL,
+  `commenter_ID` int(11) NOT NULL,
+  `tekst` varchar(3000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `follows`
+--
+
+CREATE TABLE `follows` (
+  `ID` int(11) NOT NULL,
+  `followed_user_ID` int(11) NOT NULL,
+  `follower_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `ID` int(11) NOT NULL,
+  `liker_ID` int(11) NOT NULL,
+  `post_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `ID` int(11) NOT NULL,
+  `user_ID` int(11) NOT NULL,
+  `tekst` varchar(3000) NOT NULL,
+  `foto` varchar(3000) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`ID`, `user_ID`, `tekst`, `foto`) VALUES
+(1, 1, 'dgfgdfdfgdfd', '/Applications/MAMP/htdocs/IMDStagram2/postImages/dickbutt.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -25,21 +82,47 @@ CREATE TABLE `users` (
   `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `Avatar` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `Avatar` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT 'dickbutt.jpg',
+  `prive` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`ID`, `username`, `password`, `email`, `Avatar`) VALUES
-(1, 'fabio', 'fabio', 'fabio.vandooren@hotmail.com', '/Applications/MAMP/htdocs/IMDStagram2/postImages/BMW E36 CLEAN.jpg'),
-(2, 'kian', 'kian', 'kiangroene@g.com', NULL),
-(3, 'jonathan', 'jonathan', 'jonathan@g.com', NULL);
+INSERT INTO `users` (`ID`, `username`, `password`, `email`, `Avatar`, `prive`) VALUES
+(19, 'fabio', '$2y$12$AaH1CG2NexLjesGGI3Xu..GaUgWMryDb5Mc73JD.rTgNgGtdNk72y', 'fabiokjfsbdn@hotm.com', 'BMWE36CLEAN.jpg', 1),
+(20, 'kian', '$2y$12$Hv2fFQBybwtuv4wB7var8Ol4Dl86QKNjsBLjgszlHOqd9l/hmCrmm', 'kiabfsjgbdnd@sfsd.com', 'mein neger.jpg', 0),
+(27, 'jonathan', '$2y$12$/mPeGpeQXAamB4TSwqqWsuhKPhj8XK8jOOIrqGBK7s80Fl/sNjFDS', 'joan@johnny@hotmail.com', 'dickbutt.jpg', 1),
+(44, 'jona', '$2y$12$3SngqVHG1pyx3b6cZyEkiOn0/gqhAP3.6KJNwosxZFYgs5ROc0Jty', 'jbdhfsbkjd', 'dickbutt.jpg', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `follows`
+--
+ALTER TABLE `follows`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `users`
@@ -52,7 +135,27 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `follows`
+--
+ALTER TABLE `follows`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=45;
