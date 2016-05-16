e7f31ee47c1ef8f3a6841e6d247583f5477124a9
KianGroenen <kian_groenen@hotmail.com>
2016-05-16 17:35:13 +0200

    Delete working

---
+++ classes/DeletePost.class.php
@@ -0,0 +1,53 @@
+<?php
+include_once("classes/Db.class.php");
+    class DeletePost {
+
+        private $m_sUserID;
+        private $m_sID;
+
+
+        public function __set($p_sProperty, $p_vValue)
+        {
+            switch($p_sProperty)
+            {
+                case "UserID":
+                    $this->m_sUserID = $p_vValue;
+                    break;
+                case "Identiteit":
+                    $this->m_sID = $p_vValue;
+                    break;
+            }
+        }
+
+        public function __get($p_sProperty)
+        {
+            switch($p_sProperty)
+            {
+                case "UserID":
+                    return $this->m_sUserID;
+                    break;
+                case "Identiteit":
+                    return $this->m_sID;
+                    break;
+            }
+
+        }
+
+        public function Delete() {
+            $PDO = Db::getInstance();
+            $sql = "DELETE FROM posts WHERE user_ID = :userID AND ID = :ID";
+            $stmt = $PDO->prepare($sql);
+            $stmt->bindValue(':userID', $this->m_sUserID);
+            $stmt->bindValue(':ID', $this->m_sID);
+            
+            if ($stmt->execute()) {
+                //query went OK!
+            }
+            else
+            {
+                // er zijn geen query resultaten, dus query is gefaald
+                throw new Exception('Could not delete POST!');
+            }
+        }
+    }
+?>
